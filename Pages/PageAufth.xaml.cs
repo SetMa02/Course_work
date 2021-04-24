b1f5b7bffee20cae5446a174f405fd1548dd10e7
SetMa02 <maxuser99@mail.ru>
2021-04-24 13:54:39 +0700

    нужно доделать таймер кнопки

--- Pages/PageAufth.xaml.cs
+++ Pages/PageAufth.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
 
 namespace CourseMM.Pages
 {
@@ -20,13 +21,58 @@ namespace CourseMM.Pages
     /// </summary>
     public partial class PageAufth : Page
     {
+        Game_CenterEntities context;
+        int tryCount = 0;
+        Timer timer;
         public PageAufth()
         {
             InitializeComponent();
+            context = new Game_CenterEntities();
+            
         }
 
         private void btnAufth_Click(object sender, RoutedEventArgs e)
         {
+            if (txtLogin.Text != null && txtPass.Text != null)
+            {
+                    Aufthoriz();
+                if(tryCount == 5)
+                { 
+                    btnAufth.IsEnabled = false;
+                    timer = new Timer(5000);
+                    timer.Elapsed += new ElapsedEventHandler(OnTimerEvent);
+                    timer.Enabled = true;
+                    timer.Start();
+                    btnAufth.IsEnabled = true;
+                }
+            }
+        }
+
+        public void OnTimerEvent(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+            timer.Close();
+            tryCount = 0;
+           
+        }
+
+        private void Aufthoriz()
+        {
+            string login = txtLogin.Text;
+            string password = txtPass.Text;
+
+            Position position = context.Position.FirstOrDefault(
+                p => p.Login == login && p.Password == password);
+            if (position != null)
+            {
+                //переход на след. страницк
+            }
+            else
+            {
+                tryCount++;
+                MessageBox.Show("Неверный логин или пароль", "Ошибка входа!");
+            }
+
 
         }
     }
